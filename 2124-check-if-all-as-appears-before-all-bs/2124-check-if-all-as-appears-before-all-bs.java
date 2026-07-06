d939e3a0b18a56aecaedde92d1aa896ed113fc23
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-07-06 22:08:19 +0530

    Time: 2 ms (3.66%), Space: 42.7 MB (87.8%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
@@ -1,10 +1,14 @@
 class Solution {
     public boolean checkString(String s) {
-        if(!s.contains("ba")){
-            return true;
+        Stack<Character> st = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == 'a' && !st.isEmpty() && st.peek() == 'b') {
+                return false;
+            }
+            st.push(ch);
         }
-        else{
-            return false;
-        }
+        return true;
     }
 }
