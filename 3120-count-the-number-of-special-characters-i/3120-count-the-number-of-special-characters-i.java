9bea74c4ab4266e7c4fe9b695913bc0d5a1e7738
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-07-10 16:32:27 +0530

    Time: 1 ms (100%), Space: 43.5 MB (79.16%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -1,25 +1,25 @@
 class Solution {
-    public int numberOfSpecialChars(String s) {
-        boolean [] arr1 = new boolean[26];
-        boolean [] arr2 = new boolean[26];
-        for(int i = 0; i<s.length(); i++){
-            if(Character.isUpperCase(s.charAt(i))){
-                arr1[s.charAt(i)-'A'] = true;
+    public int numberOfSpecialChars(String word) {
+        boolean[] arr1 = new boolean[26];
+        boolean[] arr2 = new boolean[26];
+        for(int i = 0; i<word.length(); i++){
+            if(Character.isUpperCase(word.charAt(i))){
+                arr2[word.charAt(i) - 'A'] = true;
+
             }
             else{
-                arr2[s.charAt(i)-'a'] = true;
+                arr1[word.charAt(i) - 'a'] = true;
             }
         }
         int count = 0;
-        for(int i = 25; i>=0; i--){
-            if(arr1[i] && arr2[i] == true){
-                count++;
 
-            }
-        }
-        return count;
+        for(int i = 0; i<26; i++){
+        if(arr1[i] && arr2[i] == true){
+            count++;
+    }
+}
+return count;
+
         
     }
 }
-
-
