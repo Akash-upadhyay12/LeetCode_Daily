c879e5d215eca5a1d5916d60ff1126cf68f6ebfc
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-07-13 09:37:26 +0530

    Time: 1 ms (95.54%), Space: 42.5 MB (47.43%) - LeetHub

--- 3959-check-good-integer/3959-check-good-integer.java
+++ 3959-check-good-integer/3959-check-good-integer.java
@@ -1,26 +1,18 @@
 class Solution {
     public boolean checkGoodInteger(int n) {
-    int sum1 = 0;
-    int sum2 = 0;
-    int k = n;
-    while(n>0){
-        int digit = n % 10;
-        sum1+= digit;
-        n /= 10;
-    }
-    while(k>0){
-        int digit = k % 10;
-        digit *= digit;
-        sum2+= digit;
-        k /= 10;
-    }
-    if(sum2-sum1 >= 50){
-        return true;
-
-    }
-    else{
+        int sum =0;
+        int sum1=0;
+        int mul=1;
+        while(n>0){
+            int digit=n%10;
+         sum=digit+sum;
+            mul=digit*digit;
+            sum1=sum1+mul;
+            n=n/10;
+        }
+        if((sum1-sum)>=50){
+            return true;
+        }
         return false;
-    }
-        
     }
 }
