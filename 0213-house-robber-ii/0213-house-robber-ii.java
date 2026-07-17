7fe3548af5516dabe9a2587fe64ee1f0d2cff417
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-07-17 21:53:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,30 +1,25 @@
 class Solution {
-    public int rob(int[] nums) {
-        if(nums.length ==1){
-            return nums[nums.length-1];
-        }
-        if(nums.length == 2){
-            return Math.max(nums[nums.length-1], nums[nums.length-2]);
-        }
-        int [] dp1 = new int[nums.length];
-        dp1[0] = nums[0];
-        dp1[1] = Math.max(dp1[0], nums[1]);
-        for(int i = 2; i<nums.length-1; i++){
-            int ans1  = nums[i] + dp1[i-2];
+    public int rob(int[] arr) {
+        int [] dp1 = new int[arr.length];
+        dp1[0] = arr[0];
+        dp1[1] = Math.max(dp1[0], arr[1]);
+        for(int i = 2; i<arr.length-1; i++){
+            int ans = arr[i] + dp1[i-2];
             int ans2 = dp1[i-1];
-            dp1[i] = Math.max(ans1, ans2);
+            dp1[i] = Math.max(ans, ans2);
         }
-        int [] dp2 = new int[nums.length];
-        dp2[1] = nums[1];
-        dp2[2] = Math.max(dp2[1], nums[2]);
-        for(int i = 3; i<nums.length; i++){
-            int ans1  = nums[i] + dp2[i-2];
+        int [] dp2 = new int[arr.length];
+        dp2[0] = 0;
+        dp2[1] = arr[1];
+        dp2[2] = Math.max(dp2[1], arr[2]);
+        for(int i = 3; i<arr.length; i++){
+            int ans = arr[i] + dp2[i-2];
             int ans2 = dp2[i-1];
-            dp2[i] = Math.max(ans1, ans2);
+            dp2[i] = Math.max(ans, ans2);
         }
         return Math.max(dp1[dp1.length-2], dp2[dp2.length-1]);
+        
 
-    
         
     }
 }
