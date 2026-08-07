3710449bcc9894db1895da36ff56da7609cf9536
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-08 01:53:47 +0530

    Time: 6 ms (7.91%), Space: 45.9 MB (18.5%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,25 +1,26 @@
 class Solution {
-    void check(int [] nums, int i,int target,ArrayList<Integer> cur,List<List<Integer>> ans){
-    if(i>=nums.length){
-        if(target == 0){
-            ans.add(new ArrayList<>(cur));
-            
+    void check(int [] nums,int i, int target,  ArrayList<Integer> cur, List<List<Integer>> ans, int sum){
+        if(i==nums.length){
+        if(sum == target){
+            ans.add(new ArrayList<>(cur));;
         }
         return;
-    }
-    cur.add(nums[i]);
-    if(target>=0){
-    check(nums, i, target-nums[i], cur, ans);
-    }
-    cur.remove(cur.size()-1);
-    check(nums, i+1, target, cur,ans);
+        }
 
-}   
+        cur.add(nums[i]);
+        if(sum<=target){
+        check(nums, i,  target, cur ,ans, sum + nums[i]);
+        }
+        cur.remove(cur.size()-1);
+        check(nums, i+1, target, cur, ans, sum);
+
+
+    }
     public List<List<Integer>> combinationSum(int[] nums, int target) {
-        ArrayList<Integer> cur = new ArrayList<>();
+        ArrayList<Integer>  cur = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
-
-        check(nums, 0, target, cur, ans);
+        int sum =0;
+        check(nums,0, target, cur, ans, sum);
         return ans;
         
     }
