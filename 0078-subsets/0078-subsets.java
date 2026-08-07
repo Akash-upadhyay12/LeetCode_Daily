463ff140887355e3ac2aad1121b141e69635c2bd
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-07 17:56:03 +0530

    Time: 1 ms (86.65%), Space: 44.4 MB (6.95%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -4,17 +4,22 @@ class Solution {
             ans.add(new ArrayList<>(cur));
             return;
         }
+        //This is  for take
         cur.add(nums[i]);
         check(nums, i+1, cur, ans);
+        // This is  for not remove last elment after adding
         cur.remove(cur.size()-1);
-        check(nums, i+1, cur, ans);
+
+        // This is for Not Take
+        check(nums, i+1, cur,ans);
+
+
     }
-    public List<List<Integer>> subsets(int[] nums){
+    public List<List<Integer>> subsets(int[] nums) {
         ArrayList<Integer> cur = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
         check(nums, 0, cur, ans);
         return ans;
-
         
     }
 }
