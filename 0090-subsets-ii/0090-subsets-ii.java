50427aee9287af275c1d794927af809c78685b80
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-07 20:02:11 +0530

    Time: 3 ms (55.52%), Space: 44.6 MB (98.72%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,25 +1,23 @@
 class Solution {
-    void check(int [] nums, int i,ArrayList<Integer> cur,HashSet<List<Integer>> set){
-        if(i>=nums.length){
-            set.add(new ArrayList<>(cur));
-            return;
+    void check(int [] nums, int i,ArrayList<Integer> cur,List<List<Integer>> ans){
+        ans.add(new ArrayList<>(cur));
+        for(int j = i; j<nums.length; j++){
+            if(j != i && nums[j] == nums[j-1]){
+                continue;
+            }
+            cur.add(nums[j]);
+            check(nums, j+1, cur,ans);
+            cur.remove(cur.size()-1);
         }
-        //Same as subset 1 but the approach is quite different.
-        //Take the element 
-        cur.add(nums[i]);
-        check(nums, i+1, cur , set);
-        // remove the last element
-        cur.remove(cur.size()-1);
-        // skip or not take condition
-        check(nums, i+1, cur, set);
-    }
+       
+    }  
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        //first we try the brute force approach
-        Arrays.sort(nums); // For maintaining order of element
+        //now we are making our code faster
+        Arrays.sort(nums); 
         ArrayList<Integer> cur = new ArrayList<>();
-        HashSet<List<Integer>> set = new HashSet<>(); // for remove all the duplicates
-        check(nums, 0, cur, set);
-        return new ArrayList<>(set);
+        List<List<Integer>> ans = new ArrayList<>();
+        check(nums, 0, cur, ans);
+        return ans;
         
     }
 }
