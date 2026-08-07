2ed992fcfafd50da78d051c15e5638807ed971be
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-08 03:42:29 +0530

    Time: 13 ms (99.25%), Space: 98 MB (66.13%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,31 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-     void check(int n, int k,int i, ArrayList<Integer> cur,List<List<Integer>> ans, int[] nums){
-        if(i==nums.length){
-            if(cur.size()==k){
-                ans.add(new ArrayList<>(cur));
-            }
-                return;
-            }
-
-            cur.add(nums[i]);
-            check(n, k, i+1, cur, ans, nums);
+    void check(int k, int i, ArrayList<Integer> cur, List<List<Integer>> ans,int n) {
+        if (cur.size() == k) {
+            ans.add(new ArrayList<>(cur));
+            return;
+        }
+        for(int j = i; j<=n; j++){
+            cur.add(j);
+            check(k, j+1, cur, ans, n);
             cur.remove(cur.size()-1);
-            check(n, k, i+1, cur, ans, nums);
 
-     }
+        }
+        
+    }
+
     public List<List<Integer>> combine(int n, int k) {
         ArrayList<Integer> cur = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
-        int [] nums = new int[n];
-        int j = 0;
-        while(n>0){
-            nums[j] = n;
-            n--;
-            j++;
-        }
-        System.out.println(Arrays.toString(nums));
-        check(n, k,0, cur, ans, nums);
+        check(k, 1, cur, ans, n);
         return ans;
-
     }
 }
