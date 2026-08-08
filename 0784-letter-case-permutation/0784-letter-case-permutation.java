1ab6195d1f06b17c542ecf27de227c467968aca0
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-08 23:17:55 +0530

    Time: 2 ms (58.87%), Space: 47.4 MB (55.65%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    void check(String s, int i, StringBuilder cur, List<String> ans) {
+        if(i==s.length()){
+            ans.add(cur.toString());
+    
+            return;
+        }
+        cur.append(s.charAt(i));
+        check(s, i + 1, cur, ans);
+        cur.deleteCharAt(cur.length()-1);
+        if(Character.isUpperCase(s.charAt(i))){
+            cur.append(Character.toLowerCase(s.charAt(i)));
+            check(s, i + 1, cur, ans);
+            cur.deleteCharAt(cur.length()-1);
+        }
+        if(Character.isLowerCase(s.charAt(i))){
+            cur.append(Character.toUpperCase(s.charAt(i)));
+            check(s, i + 1, cur, ans);
+            cur.deleteCharAt(cur.length()-1);
+        }
+    }
+
+    public List<String> letterCasePermutation(String s) {
+        StringBuilder cur = new StringBuilder();
+        List<String> ans = new ArrayList<>(); 
+        check(s, 0, cur, ans);
+        return ans;
+    }
+}
