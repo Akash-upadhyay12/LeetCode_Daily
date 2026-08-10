5fd56f16d3e6082079b8a03e9062fcc019a09de6
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-10 14:37:31 +0530

    Time: 1 ms (87.19%), Space: 45.3 MB (79.12%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,28 +1,27 @@
 class Solution {
-    void check(int [] nums, int i, ArrayList<Integer> cur,  List<List<Integer>> ans, boolean [] use){
-        if(cur.size()==nums.length){
-            ans.add(new ArrayList<>(cur));
-            return;
-        }
-        for(int j = 0; j<nums.length; j++){
-            if(use[j] == false){
-                cur.add(nums[j]);
-                use[j] = true;
-                check(nums, j+1, cur, ans, use);
-                cur.remove(cur.size()-1);
-                use[j] = false;
+    void check(int [] nums,List<Integer> cur, List<List<Integer>> ans, boolean[] arr){
+            if(cur.size() == nums.length){
+                ans.add(new ArrayList<>(cur));
+                return;
             }
-            
-        }
-
+            for(int j = 0; j<nums.length; j++){
+                if(arr[j]== false){
+                    cur.add(nums[j]);
+                    arr[j] = true;
+                    check(nums, cur, ans, arr);
+                    cur.remove(cur.size()-1);
+                    arr[j] = false;
+                }
+            }          
 
     }
     public List<List<Integer>> permute(int[] nums) {
-        ArrayList<Integer> cur = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
-        boolean [] use = new boolean[nums.length];
-        check(nums, 0, cur, ans, use);
-        return ans;
+        boolean [] arr = new boolean[nums.length];
+        check(nums,cur, ans, arr);
+        return  ans;
+        
         
     }
 }
