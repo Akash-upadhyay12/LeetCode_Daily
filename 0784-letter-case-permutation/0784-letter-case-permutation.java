5c50a1e4c272484e4ac06af02db866fe98ebb43a
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-11 00:07:18 +0530

    Time: 2 ms (57.98%), Space: 47.6 MB (24.01%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.java
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -1,29 +1,38 @@
-import java.util.*;
 class Solution {
-    void check(String s, int i, StringBuilder cur, List<String> ans) {
-        if(cur.length() == s.length()){
+    void check(String s, int i, StringBuilder cur, List<String> ans){
+       // if(i==s.length()){
+        if(cur.length()==s.length()){
             ans.add(cur.toString());
+       // }
             return;
         }
         cur.append(s.charAt(i));
-        check(s, i + 1, cur, ans);
+        check(s, i+1, cur, ans);
         cur.deleteCharAt(cur.length()-1);
         if(Character.isUpperCase(s.charAt(i))){
+            if(Character.isDigit(s.charAt(i))){
+            cur.append(s.charAt(i));
+        }
             cur.append(Character.toLowerCase(s.charAt(i)));
-            check(s, i + 1, cur, ans);
+            check(s, i+1, cur, ans);
             cur.deleteCharAt(cur.length()-1);
         }
         if(Character.isLowerCase(s.charAt(i))){
+            if(Character.isDigit(s.charAt(i))){
+            cur.append(s.charAt(i));
+        }
             cur.append(Character.toUpperCase(s.charAt(i)));
-            check(s, i + 1, cur, ans);
+            check(s, i+1, cur, ans);
             cur.deleteCharAt(cur.length()-1);
         }
-    }
+        
 
+    }
     public List<String> letterCasePermutation(String s) {
         StringBuilder cur = new StringBuilder();
-        List<String> ans = new ArrayList<>(); 
+        List<String> ans = new ArrayList<>();
         check(s, 0, cur, ans);
         return ans;
+        
     }
 }
