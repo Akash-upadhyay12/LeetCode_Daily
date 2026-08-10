c19d175e110012a4105ee1da02cf7d87a6ad97a9
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-11 00:38:05 +0530

    Time: 8 ms (74.13%), Space: 65.4 MB (41.06%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,12 +1,11 @@
 class Solution {
-    void check(String s, int i,ArrayList<String> cur, List<List<String>> ans){
-        //This is my first helper function for add valid palindrome to list
+    void check(String s,int i , List<String> cur, List<List<String>> ans){
         if(i==s.length()){
             ans.add(new ArrayList<>(cur));
             return;
         }
-        for(int j = i; j<s.length(); j++){
-            if(func(s, i, j)){
+        for(int j =i; j<s.length(); j++){
+            if(found(s, i, j)){
                 cur.add(s.substring(i, j+1));
                 check(s, j+1, cur, ans);
                 cur.remove(cur.size()-1);
@@ -14,24 +13,22 @@ class Solution {
         }
 
     }
-    boolean func(String s, int l, int r){
-        // This is my second helper function for validating the string
-        while(l<=r){
-            if(s.charAt(l) != s.charAt(r)){
+    boolean found(String s, int left, int right){
+        
+        while(left<=right){
+            if(s.charAt(left) != s.charAt(right)){
                 return false;
             }
-            l++;
-            r--;
+            left++;
+            right--;
         }
         return true;
-
-
     }
     public List<List<String>> partition(String s) {
-        ArrayList<String> cur = new ArrayList<>();
+        int left = 0;
+        int right = s.length()-1;
+        List<String> cur = new ArrayList<>();
         List<List<String>> ans = new ArrayList<>();
-        int l = 0;
-        int r = s.length()-1;
         check(s, 0, cur, ans);
         return ans;
         
