f5e10b3ebaf23aefa54d23ab6d8d1f6e69a24b09
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-13 18:47:38 +0530

    Time: 59 ms (31.47%), Space: 126 MB (13.27%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
@@ -1,40 +1,51 @@
 class Solution {
     public long gcdSum(int[] nums) {
         int max = 0;
-        int [] maxnum = new int[nums.length];
-        for(int i = 0; i<nums.length; i++){
-            max = Math.max(max, nums[i]);
-            maxnum[i] = max;
-        }
-        int[] pg = new int[nums.length];
-        for(int j = 0; j<nums.length; j++){
-            int a = nums[j];
-            int b = maxnum[j];
-            while(b != 0){
-            int temp = b;
-            b = a % b;
-            a = temp;
-            }
-            pg[j] = a;
-        }
-        Arrays.sort(pg);
-        int i = 0;
-        int j = pg.length-1;
-        long ans = 0;
-        while(i<j){
-            int a = pg[i];
-            int  b= pg[j];
-            while(b != 0){
-            int temp = b;
-            b = a % b;
-            a = temp;
-            }
-            ans +=(long)a;
-            i++;
-            j--;
-            
-        }
-        return ans;
+        int[] arr = new int[nums.length];
         
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                arr[i] = nums[i];
+                max = nums[i];
+            } 
+            else {
+                arr[i] = max; 
+            }
+        }
+
+        int[] ans = new int[nums.length];
+        int i = 0, j = 0, k = 0;
+        while (i < nums.length && j < arr.length) {
+            int a = nums[i];
+            int b = arr[j];
+            while (b != 0) {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            ans[k] = a;
+            i++; j++; k++;
+        }
+        
+        Arrays.sort(ans);
+        
+        int x = 0;
+        int y = ans.length - 1;
+        long sum2 = 0;
+        while (x < y) {
+            long a = ans[x];
+            long b = ans[y]; 
+
+            while (b != 0) {
+                long temp = b;
+                b = a % b;
+                a = temp;
+            }
+            sum2 += a; 
+            x++;
+            y--;
+        }
+
+        return sum2;
     }
 }
