159dec93e42d813a5aa7e1f15099d829d8935f9c
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-14 00:12:06 +0530

    Time: 135 ms (5.34%), Space: 48.1 MB (96.44%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.java
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.java
@@ -5,17 +5,23 @@ class Solution {
             char ch = s.charAt(i);
             map.put(ch, map.getOrDefault(ch, 0) +1);
         }
-        int ans = s.length();
-        for(char ch : map.keySet()){
-            if(map.get(ch)%2 == 0){
-                ans -= (map.get(ch)-2);
+        int sum  =0;
+        HashSet<Character> set = new HashSet<>();
+        for(int  i = 0; i<s.length(); i++){
+            set.add(s.charAt(i));
+        }
+        for(char ch : set){
+            if(map.get(ch) <=2){
+                sum += map.get(ch);
             }
-            else{
-                ans -= (map.get(ch)-1);
+            else if(map.get(ch) > 2 && map.get(ch) % 2 != 0){
+                sum += 1;
             }
-        }    
-            return ans;
-            
+            else if(map.get(ch) > 2 && map.get(ch) % 2 == 0){
+                sum += 2;
+            }
+        }
+        return sum;
 
         
     }
