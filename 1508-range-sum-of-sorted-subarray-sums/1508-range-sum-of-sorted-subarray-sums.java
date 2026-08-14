4f8ff491fdac0b08fc430bc0ed53fac44830787d
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-14 21:27:55 +0530

    Time: 359 ms (5.03%), Space: 113.9 MB (6.21%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -1,22 +1,26 @@
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
         int mod = 1000000007;
-        ArrayList<Long> list = new ArrayList<>();
-        for(int i = 0; i<nums.length; i++){
-            list.add((long)nums[i]);
-            long sum =nums[i];
-           for(int j = i+1; j<nums.length; j++){
-              sum += nums[j];
-              list.add((long)sum);
-           }
-    }
-    Collections.sort(list);
-    long sum = 0;
-    for(int i = left-1; i<right; i++){
-        sum = (sum + list.get(i)) % mod;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i<nums.length-1; i++){
+            list.add(nums[i]);
+            int sum = 0;
+            for(int j = i+1; j<nums.length; j++){
+                list.add(nums[j] + list.get(list.size()-1));
+                if(i == nums.length-2){
+                    list.add(nums[j]);
+                }
+            }
+        }
+        Collections.sort(list);
+        long fans = 0;
+        for(int i = left-1; i<right; i++){
+            fans = (fans + list.get(i)) % mod;
 
-    }
-    return (int)sum;
+        }
+
+        System.out.println(list);
+        return (int)fans;
         
     }
 }
