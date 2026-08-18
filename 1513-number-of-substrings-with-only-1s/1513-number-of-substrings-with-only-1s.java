a28c082096f8e150fb3b05504941621d631aedfd
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-19 01:53:22 +0530

    Time: 5 ms (67.95%), Space: 46.6 MB (6.18%) - LeetHub

---
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numSub(String s) {
+        int mod = 1000000007;
+        int count = 0;
+        int sum = 0;
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '1'){
+                count++;
+                sum = (sum + count) % mod;
+            }
+            else{
+                count = 0;
+            }
+        }
+        return sum;
+        
+    }
+}
