9d609c919251a859c8524635191f66052d896329
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-20 19:15:14 +0530

    Time: 0 ms (100%), Space: 43.1 MB (17%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -4,10 +4,10 @@ class Solution {
 
         for(int i = 0; i<s.length(); i++){
              char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch , 0) +1);
-            if(map.get(ch) == 2){
+             if(map.containsKey(ch)){
                 return ch;
-            }
+             }
+             map.put(ch, i);
         }
         return 'a';
        
