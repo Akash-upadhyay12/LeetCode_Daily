e8884f7cd427ac50dfb64d991da243287f2788ab
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-21 22:42:55 +0530

    Time: 2 ms (99.73%), Space: 45.1 MB (42.31%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,23 +1,20 @@
 class Solution {
-    void check(int [] nums, int i,ArrayList<Integer> cur,List<List<Integer>> ans){
+    void check(int [] nums, int i, ArrayList<Integer> cur, List<List<Integer>> ans){
         ans.add(new ArrayList<>(cur));
         for(int j = i; j<nums.length; j++){
-            if(j != i && nums[j] == nums[j-1]){
-                continue;
-            }
+            if(j != i && nums[j] == nums[j-1]) continue;
             cur.add(nums[j]);
-            check(nums, j+1, cur,ans);
+            check(nums, j+1, cur, ans);
             cur.remove(cur.size()-1);
         }
-       
-    }  
+    }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        //now we are making our code faster
-        Arrays.sort(nums); 
+        Arrays.sort(nums);
         ArrayList<Integer> cur = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
         check(nums, 0, cur, ans);
         return ans;
+
         
     }
 }
