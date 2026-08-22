f47646c670cb2fea25c10e176471f9e6f9b60dd9
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-23 04:59:24 +0530

    Time: 2 ms (83.6%), Space: 43.2 MB (60.11%) - LeetHub

--- 0824-goat-latin/0824-goat-latin.java
+++ 0824-goat-latin/0824-goat-latin.java
@@ -1,35 +1,30 @@
 class Solution {
-    public String toGoatLatin(String s){
-        String [] words = s.split(" ");
+    public String toGoatLatin(String s) {
+        String[] words = s.split(" ");
         StringBuilder sb = new StringBuilder();
         int y = 1;
-        for(String w : words){
-            char [] arr = w.toCharArray();
-            if(arr[0] == 'a' || arr[0] == 'e' || arr[0] == 'i' || arr[0] == 'o' || arr[0] == 'u'||arr[0] == 'A' || arr[0] == 'E' || arr[0] == 'I' || arr[0] == 'O' || arr[0] == 'U'){
-                for(int i = 0; i<arr.length; i++){
-                    sb.append(arr[i]);
-                }
+        for (String w : words) {
+            char[] arr = w.toCharArray();
+            if (arr[0] == 'a' || arr[0] == 'e' || arr[0] == 'i' ||
+                arr[0] == 'o' || arr[0] == 'u' ||
+                arr[0] == 'A' || arr[0] == 'E' || arr[0] == 'I' ||
+                arr[0] == 'O' || arr[0] == 'U') {
+                sb.append(w);
                 sb.append("m");
-                for(int z = 0; z<=y; z++){
-                    sb.append("a");
-                }
-                sb.append(" ");
-            }
-            else if(arr[0] != 'a' || arr[0] != 'e' || arr[0] != 'i' || arr[0] != 'o' || arr[0] != 'u'||arr[0] == 'A' || arr[0] == 'E' || arr[0] == 'I' || arr[0] == 'O' || arr[0] == 'U'){
+            } else {
                 char x = arr[0];
-                for(int i = 1; i<arr.length; i++){
+                for (int i = 1; i < arr.length; i++) {
                     sb.append(arr[i]);
                 }
                 sb.append(x);
                 sb.append("m");
-                for(int z = 0; z<=y; z++){
-                    sb.append("a");
-                }
-                sb.append(" ");
             }
+            for (int z = 0; z <= y; z++) {
+                sb.append("a");
+            }
+            sb.append(" ");
             y++;
         }
         return sb.toString().trim();
-        
     }
 }
