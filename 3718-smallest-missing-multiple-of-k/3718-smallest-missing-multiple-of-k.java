654705bf882fbbc7f5ab617bf475b1f3cfe002bc
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-25 20:18:26 +0530

    Time: 2 ms (68.2%), Space: 45.3 MB (52.43%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -5,18 +5,14 @@ class Solution {
             map.put(x, 1);
         }
         int a = k;
-        for(int x : nums){
+        boolean found = false;
+        while(found == false){
             if(!map.containsKey(a)){
                 return a;
             }
             a += k;
         }
-        if(map.containsKey(k*2)){
-            return a;
-        }
-        else{
-            return k * 2;
-        }
+        return -1;
 
         
     }
