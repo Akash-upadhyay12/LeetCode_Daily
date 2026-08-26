d04f464b0d86a641170676daa93db8979988a7f3
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-08-26 22:09:10 +0530

    Time: 1 ms (94.3%), Space: 45.5 MB (82.26%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
@@ -1,13 +1,17 @@
 class Solution {
     public int findFinalValue(int[] nums, int x) {
-        Arrays.sort(nums);
-        int a = x;
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i]  == a){
-                a *= 2;
-            }
+       int i = 0;
+       while(i<nums.length){
+        if(nums[i] == x){
+            x *=2;
+            i = 0;
         }
-        return a;
+        else{
+            i++;
+        }
+
+       }
+       return x;
 
         
         
