7ffb423ad5084fad6c326207a70b0b4660dce2b3
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-09-14 23:43:48 +0530

    Time: 76 ms (5.61%), Space: 47.2 MB (7.98%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -4,9 +4,8 @@ class Solution {
         for(int x : arr){
             map.put(x, map.getOrDefault(x, 0) + 1);
         }
-        HashMap<Integer, Integer> map1 = new HashMap<>();
         int count = 0;
-        for(int i = 100; i < 999; i++){
+        for(int i = 100; i <= 999; i++){
             HashMap<Integer, Integer> map2 = new HashMap<>();
             if(i % 2 == 0){
                 int temp = i;
@@ -15,19 +14,16 @@ class Solution {
                     map2.put(digit, map2.getOrDefault(digit, 0) + 1);
                     temp /= 10;
                 }
-                if(!map1.containsKey(i)){
-                    boolean possible = true;
+                    boolean found = true;
                     for(int curr : map2.keySet()) {
                         if(!map.containsKey(curr) || map.get(curr) < map2.get(curr)) {
-                            possible = false;
+                            found = false;
                             break;
                         }
                     }
-                    if(possible){
+                    if(found){
                         count++;
-                        map1.put(i, 0);
                     }
-                }
             }
         }
 
