663355fd1613b324f41be54b0dcaba4abed8d49f
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-09-16 16:53:38 +0530

    Time: 1 ms (79.66%), Space: 47.7 MB (22.71%) - LeetHub

---
+++ 2923-find-champion-i/2923-find-champion-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findChampion(int[][] mat) {
+        int max = 0;
+        int a = 0;
+        for(int i = 0; i<mat.length; i++){
+            int sum = 0;
+            for(int j = 0; j<mat[0].length; j++){
+                sum += mat[i][j];
+            }
+            if(sum > max){
+                max = sum;
+                a = i;;
+            }
+        }
+        return a;
+        
+        
+    }
+}
