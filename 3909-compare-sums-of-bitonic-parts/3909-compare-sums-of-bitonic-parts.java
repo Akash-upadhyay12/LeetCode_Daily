e7dc431f0de9411bbad9742fe10eae468c451452
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-09-22 22:35:02 +0530

    Time: 2 ms (79.37%), Space: 103.9 MB (84.66%) - LeetHub

---
+++ 3909-compare-sums-of-bitonic-parts/3909-compare-sums-of-bitonic-parts.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int compareBitonicSums(int[] nums) {
+        int idx = 0;
+        long max = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+                idx = i;
+            }
+        }
+
+        long sum1 = 0;
+        for (int i = 0; i <= idx; i++) {
+            sum1 += nums[i];
+        }
+
+        long sum2 = 0;
+        for (int i = idx; i < nums.length; i++) {
+            sum2 += nums[i];
+        }
+
+        if (sum1 > sum2) {
+            return 0;
+        }
+
+        if (sum2 > sum1) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
