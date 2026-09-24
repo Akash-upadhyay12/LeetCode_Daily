2c362f4c7c6f4993ad0896497d4b0cf39887887f
Akash-upadhyay12 <aagamerstudy@gmail.com>
2026-09-24 14:23:22 +0530

    Time: 2357 ms (15.45%), Space: 52.8 MB (30.47%) - LeetHub

--- 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.java
+++ 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.java
@@ -1,29 +1,21 @@
 class Solution {
-    public int[] sumEvenAfterQueries(int[] nums, int[][] mat) {
-        
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i = 0; i<mat.length; i++){
-            nums[mat[i][1]] += mat[i][0];
+    public int[] sumEvenAfterQueries(int[] nums, int[][] qu){
+        int [] arr = new int[qu.length];
+        int i = 0;
+        for(int [] q : qu){
+            int val = q[0];
+            int idx =q[1];
+            nums[idx] += val;
             int sum = 0;
-            for(int j = 0; j<nums.length; j++){
-                if(nums[j] % 2 == 0){
-                    sum += nums[j];
-
+            for(int x : nums){
+                if(x % 2 == 0){
+                    sum += x;
                 }
             }
-            list.add(sum);
+            arr[i] = sum;
+            i++;
         }
-        int [] arr = new int[list.size()];
-        int k = 0;
-        for(int x : list){
-            arr[k] = x;
-            k++;
-        }
-        System.out.println(list);
         return arr;
-
-       
-
         
     }
 }
